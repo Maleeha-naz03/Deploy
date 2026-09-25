089d06ad0b465ae01e73dc98b0dee030c3faf4f9
Maleeha-naz03 <zaeemmuhammad888@gmail.com>
2026-09-25 10:14:20 +0500

    Done

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/My-React-App/",
+  base: "/repo-project/",
   plugins: [react(), tailwindcss()],
 })
 
